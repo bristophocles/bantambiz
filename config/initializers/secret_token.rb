5fc5d24501135ba1beb6bf4876b642fd13abb793
bristophocles <bristophocles@gmail.com>
2013-04-28 17:45:12 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bantambiz::Application.config.secret_token = 'e6b553e40bd9a7aa0914365f11d4d61f7acda83150b0d0d9761919f178003c098c195eae661495535a0a125ba10ab89d3ab641ac957e757ecebfa3123b375c2a'
